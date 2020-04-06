d349afcd2d0d16de7f8dd65f01b41370b8a43e8a
Kruce Blake <KruceBlake@gmail.com>
2020-04-06 18:56:22 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,8 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
 var bot = new Discord.Client({
    token: process.env.BOT_TOKEN,
    autorun: true
 });
 bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
     if (message.substring(0, 1) == '!') {
