a09157bd4691e8e9ab6de55650b892bce1e065bf
Kruce Blake <KruceBlake@gmail.com>
2020-04-13 18:24:00 -0400

    Update overwatch.js

--- overwatch.js
+++ overwatch.js
@@ -48,3 +48,15 @@ module.exports = {
         }
     }
 };
+
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) { // while there remain elements to shuffle...
+        randomIndex = Math.floor(Math.random() * currentIndex); // pick a remaining element...
+        --currentIndex;
+        temporaryValue = array[currentIndex]; // and swap it with the current element.
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
