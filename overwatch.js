1fb56b332331ba6fa5ad5268f27f792aea185f63
Kruce Blake <KruceBlake@gmail.com>
2020-04-13 13:07:58 -0400

    removed wordcount function; replaced -=1 to -- in shuffle func;

--- overwatch.js
+++ overwatch.js
@@ -1,15 +1,14 @@
 module.exports = {
     assign: function (content) {
-        if (WordCount(content) <= 6 && WordCount(content) >= 1) {
+        let players = content.trim().replace(/[ ]{2,}/gi, ` `).split(` `); //trim empty space, replace spaces more than one with one, split by word
+        if (players.length <= 6 && players.length >= 1) {
             let message = ``;
             let roles = [`tank`, `tank`, `damage`, `damage`, `support`, `support`];
             let heroes = {
-                "tank": [`d.va`, `orisa`, `reinhardt`, `roadhog`, `sigma`, `winston`, `wrecking ball`, `zarya`],
+                "tank": [`d.va`, `orisa`, `reinhardt`, `roadhog`, `sigma`, `winston`, `wrecking ball`],
                 "damage": [`ashe`, `bastion`, `doomfist`, `genji`, `hanzo`, `junkrat`, `mcree`, `mei`, `pharah`, `reaper`, `soldier: 76`, `sombra`, `symmetra`, `torbjörn`, `tracer`, `widowmaker`],
                 "support": [`ana`, `baptiste`, `brigitte`, `lúcio`, `mercy`, `moira`, `zenyatta`]
             };
-
-            let players = content.split(` `).filter(function (str) { return /\S/.test(str); }); //remove empty whitespaces
 
             roles = shuffle(roles); //shuffle roles and heroes
             heroes["tank"] = shuffle(heroes["tank"]);
@@ -17,7 +16,7 @@ module.exports = {
             heroes["support"] = shuffle(heroes["support"]);
 
             while (players.length > 0) {
-                message += `**${(players[0]).replace(`%20`,` `)}:** {${roles[0]}, ${heroes[roles[0]][0]}} `; //replace %20 with space
+                message += `**${(players[0]).replace(`%20`, ` `)}:** {${roles[0]}, ${heroes[roles[0]][0]}} `; //replace %20 with space
                 heroes[roles[0]].shift();
                 roles.shift();
                 players.shift();
@@ -30,22 +29,11 @@ module.exports = {
     }
 };
 
-function WordCount(str) {
-    let totalSoFar = 0;
-    let words = str.split(` `);
-    for (var i = 0; i < words.length; i++) {
-        if (words[i] !== ` ` && words[i] !== ``) {
-            totalSoFar++;
-        }
-    }
-    return totalSoFar;
-}
-
 function shuffle(array) {
     let currentIndex = array.length, temporaryValue, randomIndex;
     while (0 !== currentIndex) { // while there remain elements to shuffle...
         randomIndex = Math.floor(Math.random() * currentIndex); // pick a remaining element...
-        currentIndex -= 1;
+        --currentIndex;
         temporaryValue = array[currentIndex]; // and swap it with the current element.
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
