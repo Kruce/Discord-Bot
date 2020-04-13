137a3af503f3ee40040a3f441c84ba396e9d7cb3
Kruce Blake <KruceBlake@gmail.com>
2020-04-13 18:16:00 -0400

    updated to allow choosing a random hero if you enter a role;

--- overwatch.js
+++ overwatch.js
@@ -1,25 +1,45 @@
 module.exports = {
     assign: function (content) {
-        let players = content.trim().replace(/[ ]{2,}/gi, ` `).split(` `); //trim empty space, replace spaces more than one with one, split by word
-        if (players.length <= 6 && players.length >= 1) {
+        let words = content.trim().toLowerCase().replace(/[ ]{2,}/gi, ` `).split(` `); //trim empty space, replace spaces more than one with one, split by word
+        if (words.length <= 6 && words.length >= 1) {
             let message = ``;
-            let roles = [`tank`, `tank`, `damage`, `damage`, `support`, `support`];
+            let reservedRoles = []; //if they are just requesting a hero change, the role gets reserved
+            let remainedRoles = [`tank`, `tank`, `damage`, `damage`, `support`, `support`];
             let heroes = {
                 "tank": [`d.va`, `orisa`, `reinhardt`, `roadhog`, `sigma`, `winston`, `wrecking ball`, `zarya`],
                 "damage": [`ashe`, `bastion`, `doomfist`, `genji`, `hanzo`, `junkrat`, `mcree`, `mei`, `pharah`, `reaper`, `soldier: 76`, `sombra`, `symmetra`, `torbjörn`, `tracer`, `widowmaker`],
                 "support": [`ana`, `baptiste`, `brigitte`, `lúcio`, `mercy`, `moira`, `zenyatta`]
             };
 
-            while (players.length > 0) {
-                roles = shuffle(roles); //shuffle roles and heroes. shuffling inside while loop for extra randomness
+            let tempWords = [...words]; //create an array clone of all our words
+            while (tempWords.some(r => remainedRoles.indexOf(r) >= 0)) { //while our user's words contain any remained role
+                for (let role of tempWords) { 
+                    let index = remainedRoles.indexOf(role); //find the index of role in remained roles
+                    if (index > -1) { //if it exists, remove it from remained roles and add it to reserved roles
+                        let element = remainedRoles[index];
+                        remainedRoles.splice(index, 1);
+                        reservedRoles.push(element);
+                    }
+                    tempWords.splice(tempWords.findIndex(r => r == role), 1);
+                }
+            }
+            while (words.length > 0) { //shuffling roles and heroes inside while loop for extra randomness
                 heroes["tank"] = shuffle(heroes["tank"]);
                 heroes["damage"] = shuffle(heroes["damage"]);
                 heroes["support"] = shuffle(heroes["support"]);
 
-                message += `**${(players[0]).replace(`%20`, ` `)}:** {${roles[0]}, ${heroes[roles[0]][0]}} `; //replace %20 with space
-                heroes[roles[0]].shift();
-                roles.shift();
-                players.shift();
+                if (reservedRoles.includes(words[0])) { //if the message is a role and there's still roles of that type left to use
+                    message += `{**new ${words[0]} hero**: ${heroes[words[0]][0]}} `;
+                    heroes[words[0]].shift();
+                    reservedRoles.splice(reservedRoles.findIndex(x => x == words[0]), 1); //remove the first instance of the role
+                }
+                else {
+                    remainedRoles = shuffle(remainedRoles); 
+                    message += `**${(words[0]).replace(`%20`, ` `)}:** {${remainedRoles[0]}, ${heroes[remainedRoles[0]][0]}} `; //replace %20 with space
+                    heroes[remainedRoles[0]].shift();
+                    remainedRoles.shift();
+                }
+                words.shift();
             }
             return (message);
         }
