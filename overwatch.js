ed2eb3de92787eb3b8593e9e01d78220602e0821
Kruce Blake <KruceBlake@gmail.com>
2020-04-14 14:50:33 -0400

    Update overwatch.js

--- overwatch.js
+++ overwatch.js
@@ -7,7 +7,7 @@ module.exports = {
             let remainedRoles = [`tank`, `tank`, `damage`, `damage`, `support`, `support`];
             let heroes = {
                 "tank": [`d.va`, `orisa`, `reinhardt`, `roadhog`, `sigma`, `winston`, `wrecking ball`, `zarya`],
-                "damage": [`ashe`, `bastion`, `doomfist`, `genji`, `hanzo`, `junkrat`, `mcree`, `mei`, `pharah`, `reaper`, `soldier: 76`, `sombra`, `symmetra`, `torbjörn`, `tracer`, `widowmaker`],
+                "damage": [`ashe`, `bastion`, `doomfist`, `echo`, `genji`, `hanzo`, `junkrat`, `mcree`, `mei`, `pharah`, `reaper`, `soldier: 76`, `sombra`, `symmetra`, `torbjörn`, `tracer`, `widowmaker`],
                 "support": [`ana`, `baptiste`, `brigitte`, `lúcio`, `mercy`, `moira`, `zenyatta`]
             };
             for (let word of words) { //update our remained and reserved roles before we start assigning them
