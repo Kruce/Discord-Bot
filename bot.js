3ad9b3c13fb37535f082dc0c21a2098772aaed4c
Kruce Blake <kruceblake@gmail.com>
2020-07-15 21:01:00 -0400

    included holiday module;

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const fs = require(`fs`);
 const Discord = require(`discord.js`);
 const Client = new Discord.Client({ partials: [`MESSAGE`, `CHANNEL`, `REACTION`] });
 const Schedule = require(`node-schedule-tz`);
+const Holiday = require(`./modules/holiday`);
 const Shuffle = require(`./modules/shuffle`);
 
 //set our commands collection
