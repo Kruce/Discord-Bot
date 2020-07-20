9d6f7c5eb97efb1e993029ff1c2ab797ed0a1d96
Kruce Blake <kruceblake@gmail.com>
2020-07-20 17:16:26 -0400

    refactored to account for a role if case doesn't match;

--- commands/overwatch.js
+++ commands/overwatch.js
@@ -6,18 +6,10 @@ module.exports = {
     usage: `<player name> or <overwatch role>`, //how to use the command
     cooldown: 1, //cooldown on command in seconds
     execute(message, args) {
-        if (!args.length && message.channel.type === `text`) { //if not a dm and the arguments are empty, get any players currently playing overwatch and use them
-            for (const [key, presence] of message.guild.presences.cache) {
-                for (const activity of presence.activities) {
-                    if (activity.name.trim().toUpperCase() == `OVERWATCH`) {
-                        args.push(presence.user.username);
-                    }
-                }
-            }
-        }
-        if (!args.length || args.length > 6) return message.channel.send(`To assign, overwatch requires six or less of any combination of player names or overwatch roles.`);
-        args = args.map(v => v.toLowerCase()); //convert all user input to lower case
-        let data = ``;
+        if (!args.length && message.channel.type === `text`) //if array is empty and this isn't a dm, attempt to get all players currently playing overwatch
+            args = Array.from(message.guild.presences.cache.filter(function (p) { return p.activities.some(function (a) { return a.name.trim().toUpperCase() == `OVERWATCH` }) }), p => p[1].user.username);
+        if (!args.length || args.length > 6) //if array is empty or more than six players/roles return message
+            return message.channel.send(`To assign, overwatch requires six or less of any combination of player names or overwatch roles.`);
         let reservedroles = []; //if they are just requesting a hero change, the role gets reserved
         let remainedroles = [`tank`, `tank`, `damage`, `damage`, `support`, `support`];
         let heroes = {
@@ -25,7 +17,8 @@ module.exports = {
             "damage": [`ashe`, `bastion`, `doomfist`, `echo`, `genji`, `hanzo`, `junkrat`, `mcree`, `mei`, `pharah`, `reaper`, `soldier: 76`, `sombra`, `symmetra`, `torbjörn`, `tracer`, `widowmaker`],
             "support": [`ana`, `baptiste`, `brigitte`, `lúcio`, `mercy`, `moira`, `zenyatta`]
         };
-        for (const arg of args) { //update our remained and reserved roles before we start assigning them
+        const argumentslower = args.map(v => v.toLowerCase()); //convert all user input to lower case to help match any roles
+        for (const arg of argumentslower) { //update our remained and reserved roles before we start assigning them
             const index = remainedroles.indexOf(arg); //check if the current word is equal to a role and there are any remaining
             if (index > -1) { //if it exists, remove it from remained roles and add it to reserved roles
                 const role = remainedroles[index];
@@ -33,12 +26,12 @@ module.exports = {
                 reservedroles.push(role);
             }
         }
-        while (args.length > 0) { //shuffling roles and heroes inside while loop for extra randomness
-            const arg = args[0]; //since we shift at end of loop this will always be the current word
-            heroes["tank"] = Shuffle.ShuffleArray(heroes["tank"]);
-            heroes["damage"] = Shuffle.ShuffleArray(heroes["damage"]);
-            heroes["support"] = Shuffle.ShuffleArray(heroes["support"]);
-
+        let data = ``; //data being returned to user
+        while (argumentslower.length > 0) { 
+            for (let [key, value] of Object.entries(heroes)) { //shuffling roles and heroes inside while loop instead of outside for extra randomness
+                heroes[key] = Shuffle.ShuffleArray(value)
+            }
+            const arg = argumentslower[0]; //since we shift at end of loop this will always be the current word
             if (reservedroles.includes(arg)) { //if the message is a role and there's still roles of that type left to use
                 data += `{**new ${arg} hero**: ${heroes[arg][0]}} `;
                 heroes[arg].shift();
@@ -50,7 +43,7 @@ module.exports = {
                 heroes[remainedroles[0]].shift();
                 remainedroles.shift();
             }
-            args.shift();
+            argumentslower.shift();
         }
         message.channel.send(data).catch(e => { console.error(`overwatch command error sending message for: ${message.guild.name} id: ${message.guild.id}`, e); });
     },
