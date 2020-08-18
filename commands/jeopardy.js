16e7c8c9add0061bbe7a9144acf048bd97ce3135
Kruce Blake <kruceblake@gmail.com>
2020-08-17 21:48:17 -0400

    updated jeopardy to account for invalid questions from api; updated commands;

--- commands/jeopardy.js
+++ commands/jeopardy.js
@@ -4,52 +4,52 @@ module.exports = {
     name: `jeopardy`,
     description: `Get a jeopardy formatted trivia question and answer.`,
     aliases: [`j`], //other alias to use this command
-    usage: `<answer>, <next>, or blank to repeat the question`, //how to use the command
+    usage: `<answer>, <repeat>, or blank for a new question`,
     cooldown: 1, //cooldown on command in seconds
     execute(message, args) {
-        let GetJeopardy = new Promise((resolve, reject) => {
-            if (args[0] == `next` || message.client.jeopardy === undefined || message.client.jeopardy === null) {
-                message.client.jeopardy = new Discord.Collection();
-                https.get(`https://jservice.io/api/random`, (resp) => {
-                    let data = ``;
-                    resp.on(`data`, (chunk) => {
-                        data += chunk;
+        let cmd = (args.length) ? args[0].toLowerCase() : ``; //command if any
+        const GetOrSetJeopardyCache = () =>
+            new Promise((resolve, reject) => {
+                if (message.client.jeopardy === undefined || message.client.jeopardy === null || !cmd) {
+                    https.get(`https://jservice.io/api/random`, (response) => {
+                        let data = ``;
+                        response.on(`data`, (chunk) => {
+                            data += chunk;
+                        });
+                        response.on(`end`, () => {
+                            const jservice = JSON.parse(data)[0];
+                            if (jservice.invalid_count !== null && jservice.invalid_count >= 5) { //if question was marked as invalid more than five times, ignore and get another question
+                                resolve(`retry`);
+                            }
+                            message.client.jeopardy = new Discord.Collection();
+                            for (const property in jservice) {
+                                message.client.jeopardy.set(property, jservice[property])
+                            }
+                            resolve(`cached`);
+                        });
+                    }).on(`error`, (error) => {
+                        console.error(error.message);
+                        reject(error.message);
                     });
-                    // The whole response has been received. Print out the result.
-                    resp.on(`end`, () => {
-                        let jeopardyJson = JSON.parse(data)[0];
-                        message.client.jeopardy.set(`id`, jeopardyJson.id);
-                        message.client.jeopardy.set(`category`, jeopardyJson.category.title);
-                        message.client.jeopardy.set(`question`, jeopardyJson.question);
-                        message.client.jeopardy.set(`answer`, jeopardyJson.answer);
-                        resolve(true);
-                    });
-
-                }).on(`error`, (err) => {
-                    console.error(err.message);
-                    reject(false);
-                });
+                }
+                else { resolve(`cached`); }
+            });
+        const GetJeopardyQuestion = () => //loop until a proper question is cached
+            GetOrSetJeopardyCache().then(result => {
+                if (result === `retry`) {
+                    return GetJeopardyQuestion();
+                }
+            })
+        GetJeopardyQuestion().then(() => {
+            if (cmd == `answer`) {
+                let answer = message.client.jeopardy.get(`answer`);
+                return message.channel.send(`${answer.charAt(0).toUpperCase()}${answer.slice(1)}`).catch(e => { console.error(`jeopardy command issue sending message:`, e); });
             }
             else {
-                resolve(true);
+                let category = message.client.jeopardy.get(`category`).title;
+                let data = `\n**Category:** ${category.charAt(0).toUpperCase()}${category.slice(1)} \n**Question:** ${message.client.jeopardy.get(`question`)}`;
+                return message.channel.send(data).catch(e => { console.error(`jeopardy command issue sending message:`, e); });
             }
-        })
-        GetJeopardy.then((response) => {
-            if (response) {
-                if (args[0] == `answer`) {
-                    let answer = message.client.jeopardy.get(`answer`);
-                    message.channel.send(`${answer.charAt(0).toUpperCase()}${answer.slice(1)}`)
-                        .catch(e => { console.error(`jeopardy command issue sending message:`, e); });
-                }
-                else {
-                    let category = message.client.jeopardy.get(`category`);
-                    let data = `\n**Category:** ${category.charAt(0).toUpperCase()}${category.slice(1)} \n**Question:** ${message.client.jeopardy.get(`question`)}`;
-                    message.channel.send(data)
-                        .catch(e => { console.error(`jeopardy command issue sending message:`, e); });
-                }
-            }
-        }).catch((error) => {
-            console.error(error);
         });
     },
 };
