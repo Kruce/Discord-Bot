554c3aa145a44138e5a66d3e279bc5f361193010
Kruce Blake <kruceblake@gmail.com>
2021-03-12 15:56:15 -0500

    updated stock command; removed node modules

--- commands/stock.js
+++ commands/stock.js
@@ -1,34 +1,36 @@
-const Cheerio = require(`cheerio`);
-const Discord = require(`discord.js`);
 const Request = require(`request-promise`);
+const Discord = require(`discord.js`);
 
 module.exports = {
     name: `stock`,
-    description: `Retrieves data for a nyse symbol from google finance.`,
+    description: `Retrieves data for a stock by symbol.`,
     aliases: [`s`], //other alias to use this command
     usage: `*${process.env.PREFIX}s* gme`, //how to use the command
     cooldown: 10, //cooldown on command in seconds
     execute(message, args) {
-        if (!args.length) return message.channel.send(`Please enter a nyse symbol for current price.`);
-        if (args.length > 1) return message.channel.send(`Please only enter one nyse symbol at a time.`);
-
-        const options = { uri: `https://www.google.com/finance/quote/${args}:NYSE`, transform: function (body) { return Cheerio.load(body); } };
-        Request(options)
-            .then(function ($) {
-                const main = $(`main`).children();
-                const info = main[1].firstChild.firstChild.firstChild.firstChild.firstChild.firstChild.firstChild;
-                const name = info.firstChild.firstChild.data;
-                const price = info.lastChild.firstChild.firstChild.firstChild.firstChild.firstChild.firstChild.data;
-                const embed = new Discord.MessageEmbed()
-                    .setTitle(`Price of ${name}`)
-                    .setDescription(price)
-                    .setTimestamp(new Date().toUTCString());
-                message.channel.send(embed)
-                    .catch(e => { console.error(`stock command error sending message for: ${message.guild.name} id: ${message.guild.id}`, e); });
-            })
-            .catch(function (e) {
-                console.error(`stock command error:`, e);
-                message.channel.send(`There was an error scraping google finance for that nyse symbol.`);
+        if (!args.length) return message.channel.send(`Please enter a symbol to retrieve current data.`);
+        if (args.length > 1) return message.channel.send(`Please only enter one symbol at a time.`);
+        const symbol = args[0].toUpperCase();
+        Request(`https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${process.env.FINNHUBTOKEN}`, { json: true }, (err, res, body) => {
+            return body;
+        }).then(function (price) {
+            Request(`https://finnhub.io/api/v1/stock/profile2?symbol=${symbol}&token=${process.env.FINNHUBTOKEN}`, { json: true }, (err, res, body) => {
+                if (body.name == undefined) {
+                    return message.channel.send(`There is an issue retrieving that stock data.`).catch(e => { console.error(`stock command issue sending message:`, e); });
+                }
+                else {
+                    const embed = new Discord.MessageEmbed()
+                        .setTitle(`Estimated price of ${body.name} (${body.ticker})`)
+                        .setURL(body.weburl)
+                        .setDescription(`$${price.c.toFixed(2)}`)
+                        .setTimestamp(new Date().toUTCString());
+                    return message.channel.send(embed).catch(e => { console.error(`stock command issue sending message:`, e); });
+                }
             });
+        })
+        .catch(function (e) {
+            console.error(e);
+            return message.channel.send(`There is an issue retrieving that stock data.`).catch(e => { console.error(`stock command issue sending message:`, e); });
+        })
     },
 };
