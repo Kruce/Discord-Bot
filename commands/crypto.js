d2b9d14375e077806d47f3888561b78328beb13b
Kruce Blake <kruceblake@gmail.com>
2021-04-02 12:09:50 -0400

    allow for multiple symbols in crypto command;

--- commands/crypto.js
+++ commands/crypto.js
@@ -4,14 +4,13 @@ const Number = require(`../modules/number.js`);
 
 module.exports = {
     name: `crypto`,
-    description: `retrieves crypto data for a given symbol.`,
+    description: `retrieves crypto data for any number of symbols.`,
     aliases: [`c`], //other alias to use this command
-    usage: `*${process.env.COMMAND_PREFIX}c* [crypto symbol]`, //how to use the command
+    usage: `*${process.env.COMMAND_PREFIX}c* [crypto symbol], *${process.env.COMMAND_PREFIX}c* [crypto symbol] [crypto symbol] [crypto symbol]`, //how to use the command
     args: true, //arguments are required.
     cooldown: 5, //cooldown on command in seconds
     execute(message, args) {
-        if (!args) return message.channel.send(`Please enter a symbol to retrieve current data.`);
-        if (args.length > 1) return message.channel.send(`Please only enter one symbol at a time.`);
+        if (!args) return message.channel.send(`please enter a symbol to retrieve current data.`);
         const symbol = args[0].toUpperCase();
         Fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${symbol}`, {
             headers: ({
@@ -21,26 +20,28 @@ module.exports = {
         })
         .then(response => CheckStatus(response))
         .then(function (response) {
-            const coinKey = Object.keys(response.data)[0];
-            if (!coinKey) {
+            const keys = Object.keys(response.data);
+            if (!keys || keys.length < 1) {
                 return Promise.reject(response);
             }
             else {
-                const coin = response.data[coinKey];
-                const embed = new Discord.MessageEmbed()
-                    .setTitle(`Estimated price of ${coin.name} (${coin.symbol})`)
-                    .setDescription(`Circulating supply: ${Number.CommaString(coin.circulating_supply)} / ${Number.CommaString(coin.max_supply)}`)
-                    .setTimestamp(new Date().toUTCString())
-                    .addFields(
-                        { name: 'Market Cap', value: `$${Number.DecimalString(coin.quote.USD.market_cap)}` },
-                        { name: 'Price', value: `$${Number.DecimalString(coin.quote.USD.price)}` },
-                    );
-                return message.channel.send(embed).catch(e => { console.error(`crypto command issue sending message:`, e); });
+                for (i = 0; i < keys.length; ++i) {
+                    const coin = response.data[keys[i]];
+                    const embed = new Discord.MessageEmbed()
+                        .setTitle(`Estimated price of ${coin.name} (${coin.symbol})`)
+                        .setDescription(`Circulating supply: ${Number.CommaString(coin.circulating_supply)} / ${Number.CommaString(coin.max_supply)}`)
+                        .setTimestamp(new Date().toUTCString())
+                        .addFields(
+                            { name: 'Market Cap', value: `$${Number.DecimalString(coin.quote.USD.market_cap)}` },
+                            { name: 'Price', value: `$${Number.DecimalString(coin.quote.USD.price)}` },
+                        );
+                    message.channel.send(embed).catch(e => { console.error(`crypto command issue sending message:`, e); });
+                }
             }
         })
         .catch(function (error) {
             console.error(error);
-            return message.reply(`There is an issue retrieving data for that symbol.`).catch(e => { console.error(`crypto command issue sending message:`, e); });
+            return message.reply(`there is an issue retrieving data for that symbol.`).catch(e => { console.error(`crypto command issue sending message:`, e); });
         });
         const CheckStatus = (response) => {
             if (response.ok) {
