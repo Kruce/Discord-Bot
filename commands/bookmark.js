36889f8c9558fdf88bc5c15af790fe7753b1f576
Kruce Blake <kruceblake@gmail.com>
2021-04-06 16:30:49 -0400

    removed need to add get when using bookmark command;

--- commands/bookmark.js
+++ commands/bookmark.js
@@ -5,76 +5,62 @@ module.exports = {
     name: `bookmark`,
     description: `get or set a value or display all key/value pairs that are bookmarked`,
     aliases: [`bm`], //other alias to use this command
-    usage: `*${process.env.COMMAND_PREFIX}bm get* [bookmark key], *${process.env.COMMAND_PREFIX}bm set* [bookmark key] [bookmark value], *${process.env.COMMAND_PREFIX}bm all*`, //how to use the command
+    usage: `*${process.env.COMMAND_PREFIX}bm* [bookmark key], *${process.env.COMMAND_PREFIX}bm set* [bookmark key] [bookmark value], *${process.env.COMMAND_PREFIX}bm all*`, //how to use the command
     args: true, //arguments are required.
     cooldown: 1, //cooldown on command in seconds
     execute(message, args) {
         const cmd = (args[0]) ? args[0].toLowerCase() : ``;
-        if (['set', 'get', 'all'].indexOf(cmd) < 0) { //they passed a command but it is not set or get
-            let reply = `the provided arguments are invald, ${message.author}.`;
-            if (this.usage) {
-                reply += `\n\`the proper usage would be:\` ${this.usage}`;
+        switch (cmd) {
+            case `set`: {
+                const key = (args[1]) ? args[1].toLowerCase() : ``;
+                const value = args.join(` `).slice(cmd.length + key.length + 2);
+                if (!key || !value) { //they want to set a bookmark but did not pass proper arguments
+                    let reply = `the provided arguments are invald, ${message.author}.`;
+                    if (this.usage) {
+                        reply += `\n\`the proper usage would be:\` ${this.usage}`;
+                    }
+                    return message.channel.send(reply);
+                } else if ([`set`, `all`].indexOf(key.toLowerCase()) > -1) {
+                    return message.channel.send(`that key is restricted, please try again with a different key`);
+                } else {
+                    const value = //remove cmd and key and join for value
+                        Jsonbin.ReadBin(id)
+                            .then(function (json) {
+                                const record = json.record;
+                                if (key in record) { //if key exists, ask if they want to replace value. If they do, update value.
+                                    record[key] = value;
+                                    return ReplaceKeyQuestion(record);
+                                } else { //add the new key/value
+                                    record[key] = value;
+                                    return Update(record, `key has been added.`);
+                                }
+                            })
+                            .catch((error) => {
+                                console.log(error);
+                                return message.reply(`there was an error setting bookmark`);
+                            });
+                }
+                break;
             }
-            return message.channel.send(reply);
-        } else {
-            switch (cmd) {
-                case `set`: {
-                    if (!args[1] || !args[2]) { //if missing key and/or value
-                        let msg = `you must provide `;
-                        if (!args[1] && !args[2]) {
-                            msg += `a key and a value`;
-                        } else if (!args[1]) {
-                            msg += `a key`;
+            case `all`: {
+                message.channel.send(`https://api.jsonbin.io/b/${id}/latest`);
+                break;
+            }
+            default: //get
+                Jsonbin.ReadBin(id)
+                    .then(function (json) {
+                        const record = json.record;
+                        if (cmd in record) {
+                            return message.channel.send(record[cmd]);
                         } else {
-                            msg += `a value`;
-                        } return message.reply(msg);
-                    }
-                    const key = args[1].toLowerCase();
-                    const value = args.join(` `).slice(cmd.length + key.length + 2); //remove cmd and key and join for value
-                    Jsonbin.ReadBin(id)
-                        .then(function (json) {
-                            const record = json.record;
-                            if (key in record) { //if key exists, ask if they want to replace value. If they do, update value.
-                                record[key] = value;
-                                return ReplaceKeyQuestion(record);
-                            } else { //add the new key/value
-                                record[key] = value;
-                                return Update(record, `key has been added.`);
-                            }
-                        })
-                        .catch((error) => {
-                            console.log(error);
-                            return message.reply(`there was an error setting bookmark`);
-                        });
-                    break;
-                }
-                case `get`: {
-                    if (!args[1]) { //if missing key value
-                        return message.reply(`you must provide a key`);
-                    }
-                    const key = args[1].toLowerCase();
-                    Jsonbin.ReadBin(id)
-                        .then(function (json) {
-                            const record = json.record;
-                            if (key in record) {
-                                return message.channel.send(record[key]);
-                            } else {
-                                return message.reply(`that key does not exist.`)
-                            }
-                        })
-                        .catch((error) => {
-                            console.log(error);
-                            return message.reply(`there was an error getting bookmark`);
-                        });
-                    break;
-                }
-                case `all`: {
-                    message.channel.send(`https://api.jsonbin.io/b/${id}/latest`);
-                    break;
-                }
-                default:
-                    return;
-            }
+                            return message.reply(`that key does not exist.`)
+                        }
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                        return message.reply(`there was an error getting bookmark`);
+                    });
+                break;
         }
         const ReplaceKeyQuestion = (content) =>
             message.channel.send(`key already exists. would you like to update its value?`)
