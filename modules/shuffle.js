98b0c60d035fa5cb679990bb8bc357dbffc13e13
Kruce Blake <KruceBlake@gmail.com>
2024-03-02 23:13:19 -0500

    refactored shuffle;

--- modules/shuffle.js
+++ modules/shuffle.js
@@ -3,15 +3,13 @@
 * @param {Array} array an array of items
 */
 function ShuffleArray(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
-    while (0 !== currentIndex) { // while there remain elements to shuffle...
-        randomIndex = Math.floor(Math.random() * currentIndex); // pick a remaining element...
-        --currentIndex;
-        temporaryValue = array[currentIndex]; // and swap it with the current element.
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-    return array;
+  let currentIndex = array.length,  randomIndex;
+  while (currentIndex > 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+  return array;
 }
 
 module.exports = {
