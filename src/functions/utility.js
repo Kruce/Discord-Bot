9c0264bed7ae62f7f4ca5791bab8cb8986a29821
Kruce Blake <KruceBlake@gmail.com>
2024-04-22 01:58:27 -0400

    refactored shuffle;

--- src/functions/utility.js
+++ src/functions/utility.js
@@ -44,11 +44,9 @@ const isSnowflake = (id) => {
 * @param {Array} array an array of items
 */
 const shuffleArray = (array) => {
-    let currentIndex = array.length, randomIndex;
-    while (currentIndex > 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
 }
